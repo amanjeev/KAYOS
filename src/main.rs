240efbde6b92c401fd78d5b1e062c7eebca29f75
Amanjeev Sethi <aj@amanjeev.com>
2024-10-14 10:02:09 -0400

    post 8: intro to paging

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,21 @@ pub extern "C" fn _start() -> ! {
 
     kayos::init();
 
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!(
+        "Level 4 page table at: {:?}",
+        level_4_page_table.start_address()
+    );
+
+    // let ptr = 0x2041dc as *mut u8;
+    // unsafe { let x = *ptr; }
+    // println!("read worked");
+    //
+    // unsafe { *ptr = 42; }
+    // println!("write worked");
+
     // x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
